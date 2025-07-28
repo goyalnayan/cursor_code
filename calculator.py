6a89371c9f49c19150b51a12cd5a1509b23ba2c3
Nayan Goyal <127197849+goyalnayan@users.noreply.github.com>
2025-07-28 15:17:34 +0530

    This is my first commit.

--- calculator.py
+++ calculator.py
@@ -1,2 +1,4 @@
 def add(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise TypeError('Both arguments must be integers')
     return a + b
--- test_calculator.py
+++ test_calculator.py
@@ -3,9 +3,12 @@ from calculator import add
 def test_add():
     result1 = add(2, 3)
     result2 = add(-1, 1)
-
     print(result1)  # should print 5
     print(result2)  # should print 0
+    try:
+        add('2', 3)
+    except TypeError as e:
+        print(e)  # should print error message
 
 test_add()
 
